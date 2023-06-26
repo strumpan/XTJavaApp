ba5c25a327aa7b702667a62c6980ed7ff3a2540e
Daniel Strömbrink <daniel@strumpans.com>
2023-06-26 15:28:23 +0200

    Initial commit

---
+++ src/main/java/com/xware/XTJavaApp/App.java
@@ -0,0 +1,13 @@
+package com.xware.XTJavaApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
